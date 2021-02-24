b3174bf5a290363ef3f28bf8793c80f0897cd52f
LenoM <leno855@hotmail.com>
2021-02-24 02:42:03 -0300

    Inclusão dos __tests__.

--- src/__tests__/shortenerurl.test.ts
+++ src/__tests__/shortenerurl.test.ts
@@ -1,51 +1,64 @@
-// import 'mocha';
-// import { expect } from 'chai';
-// import connection from '../config/connection';
-// import ShortenerService from '../services/ShortenerService';
+import connection from '../config/connection';
+import ShortenerService from '../services/ShortenerService';
+import ShortenerUrl from '../models/entities/ShortenerUrl';
 
-// describe('Encurtador', async () => {
-//   await connection.create();
+beforeAll(async () => {
+  const conn = await connection.create('default');
+})
 
-//   // it('Criando uma url curta sem dados validos', async () => {
-//   // const obj = await ShortenerService.create();
-//   // expect(obj.error).to.equal('Os parametros não foram preenchidos corretamente. Por favor, informe uma URL.');
-//   // });
+describe('Testando Encurtador', () => {
+  test('Criando uma url curta sem dados validos', async () => {
+    const obj = await ShortenerService.create(undefined);
+    expect(obj.error).toBe('Os parametros não foram preenchidos corretamente. Por favor, informe uma URL.');
+  });
 
-//   it('Criando uma url curta sem informar a url', async () => {
-//     const obj = await ShortenerService.create({ });
-//     expect(obj.error).to.equal('Por favor, informe uma URL.');
-//   });
+  test('Criando uma url curta sem informar a url', async () => {
+    let shortener = new ShortenerUrl();
+    const obj = await ShortenerService.create(shortener);
+    expect(obj.error).toBe('Por favor, informe uma URL.');
+  });
 
-//   it('Criando uma url curta sem uma url valida', async () => {
-//     const obj = await ShortenerService.create({ url: 'teste' });
-//     expect(obj.error).to.equal('A url informada não é valida. Por favor, verifique e tente novamente.');
-//   });
+  test('Criando uma url curta sem uma url valida', async () => {
+    let shortener = new ShortenerUrl();
+    shortener.url = 'teste';
 
-//   it('Criando uma url curta a partir de uma url valida', async () => {
-//     const obj = await ShortenerService.create({ url: 'http://www.google.com' });
-//     expect(obj.error).to.be.an('undefined');
-//     expect(obj.newUrl).toMatch(`${process.env.BASE_URL}:${process.env.PORT}/`);
-//   });
+    const obj = await ShortenerService.create(shortener);
+    expect(obj.error).toBe('A url informada não é valida. Por favor, verifique e tente novamente.');
+  });
 
-//   // it('Recuperando uma url sem uma url curta', async () => {
-//   //  const obj = await ShortenerService.get();
-//   //  expect(obj.error).to.equal('Informe a URL.');
-//   // });
+  test('Criando uma url curta a partir de uma url valida', async () => {
+    let shortener = new ShortenerUrl();
+    shortener.url = 'http://www.google.com';
 
-//   it('Recuperando uma url sem uma url curta valida', async () => {
-//     const obj = await ShortenerService.get({});
-//     expect(obj.error).to.equal('Informe a URL.');
-//   });
+    const obj = await ShortenerService.create(shortener);
+    expect(obj.error).toBe(undefined);
+    expect(obj.newUrl).toMatch(`${process.env.BASE_URL}:${process.env.PORT}/`);
+  });
 
-//   it('Recuperando uma url com uma url curta desconhecida', async () => {
-//     const obj = await ShortenerService.get('teste-teste');
-//     expect(obj.error).to.equal('URL não foi encontrada.');
-//   });
+  test('Recuperando uma url com uma url curta null', async () => {
+    const obj = await ShortenerService.get(null);
+    expect(obj.error).toBe('Informe a URL.');
+  });
 
-//   it('Recuperando uma url com uma url curta valida', async () => {
-//     const shortUrl = await ShortenerService.create({ url: 'http://www.google.com' });
-//     const obj = await ShortenerService.get(shortUrl);
-//     expect(obj.error).to.be.an('undefined');
-//     expect(obj.body).to.equal('http://www.google.com');
-//   });
-// });
+  test('Recuperando uma url com uma url curta indefinida', async () => {
+    const obj = await ShortenerService.get(undefined);
+    expect(obj.error).toBe('Informe a URL.');
+  });
+
+  test('Recuperando uma url com uma url curta desconhecida', async () => {
+    const obj = await ShortenerService.get('teste-teste');
+    
+    expect(obj.error).toBe('URL não foi encontrada.');
+  });
+
+  test('Recuperando uma url com uma url curta valida', async () => {
+    let shortener = new ShortenerUrl();
+    shortener.url = 'http://www.google.com';
+    const short = await ShortenerService.create(shortener);
+    const shortUrl = short.newUrl.replace(`${process.env.BASE_URL}:${process.env.PORT}/`, '');    
+    const obj = await ShortenerService.get(shortUrl);
+
+    expect(obj.body).toBe('http://www.google.com');
+    expect(obj.error).toBe(undefined);
+  });
+});
